6fe109f2c3d59e959a527869571767d010df32cc
Ben Schmeckpeper <bschmeck@kennasecurity.com>
2019-04-11 18:09:47 -0500

    Include a line so that the generated SVG can be seen

--- lib/svg.ex
+++ lib/svg.ex
@@ -12,6 +12,8 @@ defmodule Svg do
 \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">
 
 <svg width=\"#{w}\" height=\"#{h}\" version=\"1.1\"
-xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"></svg>"
+xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">
+<line x1=\"0\" y1=\"0\" x2=\"80\" y2=\"70\" style=\"stroke:rgb(255,0,0);stroke-width:2\" />
+</svg>"
   end
 end

9e116e3122470a3761c8690a1ea4512706fb1239
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2019-04-15 17:39:24 -0500

    Test against binaries, instead of iolists

--- test/svg_test.exs
+++ test/svg_test.exs
@@ -3,20 +3,23 @@ defmodule SvgTest do
 
   test "it renders an SVG" do
     svg = Svg.init(100, 100)
-    assert Svg.render(svg) =~ ~r/<svg.*<\/svg>/ms
+    rendered = svg |> Svg.render |> IO.iodata_to_binary
+    assert rendered =~ ~r/<svg.*<\/svg>/ms
   end
 
   test "it includes height in the SVG" do
     height = 99
     width = 88
     svg = Svg.init(width, height)
-    assert Svg.render(svg) =~ ~r/<svg .*height="#{height}"/
+    rendered = svg |> Svg.render |> IO.iodata_to_binary
+    assert rendered =~ ~r/<svg .*height="#{height}"/
   end
 
   test "it includes width in the SVG" do
     height = 99
     width = 88
     svg = Svg.init(width, height)
-    assert Svg.render(svg) =~ ~r/<svg .*width="#{width}"/
+    rendered = svg |> Svg.render |> IO.iodata_to_binary
+    assert rendered =~ ~r/<svg .*width="#{width}"/
   end
 end

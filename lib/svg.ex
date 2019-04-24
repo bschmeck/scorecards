15b791d94e69170e12a84df15962d1c91cd936a2
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2019-04-24 18:14:45 -0500

    Consolidate logic for adding elements

--- lib/svg.ex
+++ lib/svg.ex
@@ -10,12 +10,14 @@ defmodule Svg do
     %__MODULE__{width: width, height: height, elements: []}
   end
 
-  def add(svg, line = %Line{}), do: %__MODULE__{svg | elements: [line | svg.elements]}
-  def add(svg, circle = %Circle{}), do: %__MODULE__{svg | elements: [circle | svg.elements]}
-  def add(svg, text = %Text{}), do: %__MODULE__{svg | elements: [text | svg.elements]}
-  def add(svg, image = %Image{}), do: %__MODULE__{svg | elements: [image | svg.elements]}
+  def add(svg, line = %Line{}), do: add_element(svg, line)
+  def add(svg, circle = %Circle{}), do: add_element(svg, circle)
+  def add(svg, text = %Text{}), do: add_element(svg, text)
+  def add(svg, image = %Image{}), do: add_element(svg, image)
 
   def elements(%__MODULE__{elements: elts}), do: Enum.reverse(elts)
 
   def render(svg = %__MODULE__{}), do: Svg.Render.render(svg)
+
+  defp add_element(svg, elt), do: %__MODULE__{svg | elements: [elt | svg.elements]}
 end

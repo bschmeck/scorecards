113400ab185fc75074652a7acfb117d76bfca79e
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2019-04-25 09:19:27 -0500

    Refactor to use a protocol instead of pattern matching to render elements

--- lib/svg/render.ex
+++ lib/svg/render.ex
@@ -1,8 +1,11 @@
-defmodule Svg.Render do
-  alias Svg.Point
+defprotocol Svg.Render do
+  @doc "Renders an SVG element to XML"
+  def render(element, opts \\ [])
+end
 
-  def render(svg = %Svg{}) do
-    [preamble(), open_tag(svg), Enum.map(Svg.elements(svg), &render_element/1), close_tag()]
+defimpl Svg.Render, for: Svg do
+  def render(svg = %Svg{}, _opts) do
+    [preamble(), open_tag(svg), Enum.map(Svg.elements(svg), &Svg.Render.render/1), close_tag()]
   end
 
   defp preamble do
@@ -18,26 +21,35 @@ defmodule Svg.Render do
   end
 
   defp close_tag, do: ['</svg>']
+end
 
-  defp render_element(%Svg.Line{point1: pt1, point2: pt2}) do
-    ['<line', render_point(pt1, suffix: '1'), render_point(pt2, suffix: '2'),
+defimpl Svg.Render, for: Svg.Line do
+  def render(%Svg.Line{point1: pt1, point2: pt2}, _opts) do
+    ['<line', Svg.Render.render(pt1, suffix: '1'), Svg.Render.render(pt2, suffix: '2'),
      'style="stroke:rgb(255,0,0);stroke-width:2" />']
   end
+end
 
-  defp render_element(%Svg.Circle{center: pt, radius: r}) do
-    ['<circle', render_point(pt, prefix: 'c'), 'r="', Integer.to_charlist(r), '" style="stroke:black; fill:black;" />']
+defimpl Svg.Render, for: Svg.Circle do
+  def render(%Svg.Circle{center: pt, radius: r}, _opts) do
+    ['<circle', Svg.Render.render(pt, prefix: 'c'), 'r="', Integer.to_charlist(r), '" style="stroke:black; fill:black;" />']
+    end
   end
 
-  defp render_element(%Svg.Text{string: text, location: pt}) do
-    ['<text', render_point(pt), 'fill="black" style="font-family:Arial; font-size:10pt; font-weight:normal;"><![CDATA[', text, ']]></text>']
-  end
+defimpl Svg.Render, for: Svg.Text do
+  def render(%Svg.Text{string: text, location: pt}, _opts) do
+    ['<text', Svg.Render.render(pt), 'fill="black" style="font-family:Arial; font-size:10pt; font-weight:normal;"><![CDATA[', text, ']]></text>']
+    end
+end
 
-  defp render_element(%Svg.Image{url: url, location: pt, width: w, height: h}) do
-    ['<image width="', Integer.to_charlist(w), '" height="', Integer.to_charlist(h), '"', render_point(pt), 'xlink:href="', url, '" />']
+defimpl Svg.Render, for: Svg.Image do
+  def render(%Svg.Image{url: url, location: pt, width: w, height: h}, _opts) do
+    ['<image width="', Integer.to_charlist(w), '" height="', Integer.to_charlist(h), '"', Svg.Render.render(pt), 'xlink:href="', url, '" />']
   end
+end
 
-  defp render_point(pt = %Point{}), do: render_point(pt, '')
-  defp render_point(%Point{x: x, y: y}, opts) do
+defimpl Svg.Render, for: Svg.Point do
+  def render(%Svg.Point{x: x, y: y}, opts) do
     suffix = Keyword.get(opts, :suffix, '')
     prefix = Keyword.get(opts, :prefix, '')
 

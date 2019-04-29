5f519147c95bf98a0422cf78faf6538b56e36cea
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2019-04-29 08:44:26 -0500

    Add styles to circles

--- lib/svg/circle.ex
+++ lib/svg/circle.ex
@@ -1,16 +1,16 @@
 defmodule Svg.Circle do
-  defstruct [:center, :radius]
+  defstruct [:center, :radius, :style]
 
-  alias Svg.Point
+  alias Svg.{Point, Style}
   import Point, only: [is_point: 2, is_positive: 1]
 
-  def init(x, y, r) when is_point(x, y) and is_positive(r) do
-    %__MODULE__{center: %Point{x: x, y: y}, radius: r}
+  def init(x, y, r, style \\ Style.default(:circle)) when is_point(x, y) and is_positive(r) do
+    %__MODULE__{center: %Point{x: x, y: y}, radius: r, style: style}
   end
 end
 
 defimpl Svg.Render, for: Svg.Circle do
-  def render(%Svg.Circle{center: pt, radius: r}, _opts) do
-    ['<circle', Svg.Render.render(pt, prefix: 'c'), 'r="', Integer.to_charlist(r), '" style="stroke:black; fill:black;" />']
+  def render(%Svg.Circle{center: pt, radius: r, style: style}, _opts) do
+    ['<circle', Svg.Render.render(pt, prefix: 'c'), 'r="', Integer.to_charlist(r), '" ', Svg.Render.render(style), ' />']
   end
 end
--- lib/svg/style.ex
+++ lib/svg/style.ex
@@ -5,6 +5,10 @@ defmodule Svg.Style do
     %__MODULE__{stroke: Svg.Color.black, stroke_width: 2}
   end
 
+  def default(:circle) do
+    %__MODULE__{stroke: Svg.Color.black, fill: Svg.Color.black}
+  end
+
   def to_list(style) do
     style |> Map.from_struct |> Map.to_list
   end
@@ -18,4 +22,5 @@ defimpl Svg.Render, for: Svg.Style do
   defp do_render({_, nil}), do: []
   defp do_render({:stroke, color = %Svg.Color{}}), do: ['stroke: ', Svg.Render.render(color), ';']
   defp do_render({:stroke_width, w}), do: ['stroke-width:', Integer.to_charlist(w), ';']
+  defp do_render({:fill, color = %Svg.Color{}}), do: ['fill: ', Svg.Render.render(color), ';']
 end
